2a5272afefc42090434aa9be58180a93bd2559d2
MANUCON1 <manucontregar@hotmail.com>
2025-11-14 17:37:41 +0100

    Corregir import en forms.py - usar importacion relativa

--- api/forms.py
+++ api/forms.py
@@ -1,8 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
-from models import User
-
+from .models import User
 class RegisterForm(FlaskForm):
     username = StringField('Nombre de usuario', 
                           validators=[DataRequired(), Length(min=3, max=80)])
